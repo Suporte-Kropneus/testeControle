e147bfd043bc3567d8d714a0a7684b82e63806b7
Suporte-Kropneus <119679416+Suporte-Kropneus@users.noreply.github.com>
2023-12-22 15:19:52 -0300

    Implementação de CRUD

--- projeto/app/views.py
+++ projeto/app/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 from .models import Usuario
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
 
 def home(request):
     return render(request,'home.html')
 # Create your views here.
 
 def lista_usuarios(request):
+    #Exibir todos os usuarios já cadastrados em uma nova pagina
+    usuario = {
+        'usuarios': Usuario.objects.all()
+    }
+    #Retornar os dados para a página de listagem de usuários
+    return render (request,'usuarios.html',usuario)
 
-    #SALVAR OS DADOS DA TELA PARA O BANCO
+def cria_usuario(request):
+    
+        #SALVAR OS DADOS DA TELA PARA O BANCO
     novo_usuario = Usuario()
     novo_usuario.nome = request.POST.get('nome')
     novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
 
-    #Exibir todos os usuarios já cadastrados em uma nova pagina
-    usuario = {
-        'usuarios': Usuario.objects.all()
-    }
+    return redirect('/lista_usuario/')
 
-    #Retornar os dados para a página de listagem de usuários
-    return render (request,'usuarios.html',usuario)
+
+def deleta_usuario(request,pk):
+    data = get_object_or_404(Usuario,pk=pk)
+    data.delete()
+    return redirect('/lista_usuario/')
+
+
--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home, lista_usuarios
+from app.views import home, lista_usuarios,cria_usuario,deleta_usuario
 
 urlpatterns = [
     path('',home,name='home'),
-    path('usuarios/',lista_usuarios, name='listagem_usuarios')
+    path('cria_usuario/',cria_usuario, name='cria_usuarios'),
+    path('lista_usuario/',lista_usuarios, name='listagem_usuarios'),
+    path('deleta_usuario/<int:pk>/',deleta_usuario,name='deleta_usuario')
 ]
+
