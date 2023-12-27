09609c52201dbb74b3cf4b9e5a6d34522f7d5a4f
Suporte-Kropneus <119679416+Suporte-Kropneus@users.noreply.github.com>
2023-12-27 15:58:12 -0300

    Delete/Update 27/12

--- projeto/app/views.py
+++ projeto/app/views.py
@@ -32,17 +32,41 @@ def deleta_usuario(request,pk):
     data.delete()
     return redirect('/lista_usuario/')
 
+
+
 def lista_um(request):
 
 
     pk = request.GET.get('id')
-    print(pk)
     usuario_cadastrado =    {
         'usuarios':Usuario.objects.get(id_usuario=pk)    
         }
     
     return render(request,'id_usuario.html',usuario_cadastrado)
+
+def lista_dois(request):
+
+
+    pk = request.GET.get('id')
+    usuario_cadastrado =    {
+        'usuarios':Usuario.objects.get(id_usuario=pk)    
+        }
     
+    return render(request,'home.html',usuario_cadastrado)
+
+
+def deleta_usuario2(request,pk):
+    data = get_object_or_404(Usuario,pk=pk)
+    data.delete()
+
+    return redirect('/')
+
+def update(request,pk):
+
+    data = Usuario.objects.get(pk=pk)
+    return render(request,'update.html',{'data':data})
+
+
 
 
 
--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home, lista_usuarios,cria_usuario,deleta_usuario,lista_um
+from app.views import home, lista_usuarios,cria_usuario,deleta_usuario,lista_um,lista_dois,deleta_usuario2,update
 
 urlpatterns = [
     path('',home,name='home'),
     path('cria_usuario/',cria_usuario, name='cria_usuarios'),
     path('lista_usuario/',lista_usuarios, name='listagem_usuarios'),
     path('deleta_usuario/<int:pk>/',deleta_usuario,name='deleta_usuario'),
-    path('listagem_unica/', lista_um, name='listagem_unica')
+    path('listagem_unica/', lista_um, name='listagem_unica'),
+    path('homelista/',lista_dois,name='homelista'),
+    path('deletauser/<int:pk>/',deleta_usuario2,name='deletauser'),
+    path('update/<int:pk>/',update,name='update_user')
     
 ]
 
